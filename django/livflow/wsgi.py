e29c81040456c38415f733718b846ebefcbca411
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-07 04:34:00 +0900

    wsgi 경로 수정

--- django/livflow/wsgi.py
+++ django/livflow/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django.livflow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livflow.settings.product')
 
 application = get_wsgi_application()
