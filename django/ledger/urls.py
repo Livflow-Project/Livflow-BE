867187322559bc2ccdcc0b18572b9ad3005143ab
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-22 02:58:24 +0900

    함수명 변경

--- django/ledger/urls.py
+++ django/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import TransactionView, TransactionDetailView, CategoryView, CategoryDetailView
+from .views import TransactionSummaryView, TransactionDetailView, CategoryView, CategoryDetailView
 
 urlpatterns = [
     # 거래 내역 목록 조회 및 생성 엔드포인트 (GET: 목록 조회, POST: 새 거래 생성)
-    path('transactions/', TransactionView.as_view(), name='transaction-list-create'),
+    path('transactions/', TransactionSummaryView.as_view(), name='transaction-list-create'),
     
     # 특정 거래 내역 조회, 수정 및 삭제 엔드포인트 (GET: 특정 거래 조회, PUT: 수정, DELETE: 삭제)
     path('transactions/<int:id>/', TransactionDetailView.as_view(), name='transaction-detail'),
