86e6e64d07becef61f287a7f3897cf6bd65a7057
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-29 00:04:16 +0900

    serializer 수정

--- django/store/serializers.py
+++ django/store/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Store
+from rest_framework.response import Response
+from rest_framework import status
 
-
-
+# store.serializers.py
 class StoreSerializer(serializers.ModelSerializer):
     name = serializers.CharField(help_text="가게의 이름을 입력하세요.")
     address = serializers.CharField(help_text="가게의 주소를 입력하세요.")
@@ -10,4 +11,19 @@ class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
         fields = ['id', 'user', 'name', 'address']
-        read_only_fields = ['id', 'user']
+        read_only_fields = ['id']
+
+# views.py - StoreListView 클래스의 post 메소드 수정
+def post(self, request):
+    store_data = {
+        "name": request.data.get("name"),
+        "address": request.data.get("address"),
+        "user": request.user.id  # 사용자 정보를 명시적으로 store_data에 추가합니다.
+    }
+    serializer = StoreSerializer(data=store_data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
