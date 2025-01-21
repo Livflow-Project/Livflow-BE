78a9714dc2a166200ba2cb39ccd39d8345dd8834
youngkwangjoo <dudrknd1642@gmail.com>
2025-01-21 23:47:57 +0900

    fix: get store 수정

--- django/store/urls.py
+++ django/store/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import (
-    StoreListView, StoreDetailView, StoreCalendarView, 
-    TransactionCreateView, TransactionDetailView
-)
+from .views import StoreListView, StoreDetailView, StoreCalendarView
 
 urlpatterns = [
     path('', StoreListView.as_view(), name='store-list-create'),
     path('<uuid:id>/', StoreDetailView.as_view(), name='store-detail'),
     path('<uuid:id>/calendar/', StoreCalendarView.as_view(), name='store-calendar'),
-    path('<uuid:id>/transactions/', TransactionCreateView.as_view(), name='transaction-create'),
-    path('<uuid:id>/transactions/<uuid:transaction_id>/', TransactionDetailView.as_view(), name='transaction-detail'),
 ]
