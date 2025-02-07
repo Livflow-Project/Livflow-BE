4877b18796b815b8bfc178f2e8bdce4da3f8e901
youngkwangjoo <dudrknd1642@gmail.com>
2025-02-08 06:24:19 +0900

    fix: urls 수정

--- django/livflow/urls.py
+++ django/livflow/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
 # URL 패턴
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  # 'users' 앱의 URL 패턴
+    path('api/users/', include('users.urls')),  # 'users' 앱의 URL 패턴
     path('api/stores/', include('store.urls')),  # 'store' 앱의 URL 패턴 추가
     path('api/costcalcul/', include('costcalcul.urls')),  # 'costcalcul' 앱의 URL 패턴 추가
     path('api/ledger/', include('ledger.urls')),  # 'ledger' 앱의 URL 패턴 추가
