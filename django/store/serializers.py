cac4d6af5448bfc40ce2435eb5d1e21885b25e63
youngkwangjoo <dudrknd1642@gmail.com>
2025-02-12 11:47:24 +0900

    fix: store view 수정

--- django/store/serializers.py
+++ django/store/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from .models import Store
-
+from .models import Store, Transaction, Category
 
 class StoreSerializer(serializers.ModelSerializer):
     store_id = serializers.UUIDField(source='id', read_only=True)
@@ -8,3 +7,10 @@ class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
         fields = ['store_id', 'name', 'address']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    transaction_id = serializers.UUIDField(source='id', read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = ['transaction_id', 'transaction_type', 'category', 'description', 'amount', 'date']
