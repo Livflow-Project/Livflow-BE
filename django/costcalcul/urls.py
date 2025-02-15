a09d122ae0b8a7b5b23141a1bcdcf24705b590ae
youngkwangjoo <dudrknd1642@gmail.com>
2025-02-15 23:29:21 +0900

    fix: url 수정

--- django/costcalcul/urls.py
+++ django/costcalcul/urls.py
@@ -2,15 +2,13 @@ from django.urls import path
 from .views import (
     StoreRecipeListView,
     StoreRecipeDetailView,
-    StoreRecipeCreateView,
-    StoreRecipeUpdateView,
-    StoreRecipeDeleteView
+    StoreRecipeCreateView
 )
 
 urlpatterns = [
-    path('costcalcul/<int:store_id>/', StoreRecipeListView.as_view(), name='store-recipe-list'),
-    path('costcalcul/<int:store_id>/<int:recipe_id>/', StoreRecipeDetailView.as_view(), name='store-recipe-detail'),
-    path('costcalcul/<int:store_id>/', StoreRecipeCreateView.as_view(), name='store-recipe-create'),
-    path('costcalcul/<int:store_id>/<int:recipe_id>/', StoreRecipeUpdateView.as_view(), name='store-recipe-update'),
-    path('costcalcul/<int:store_id>/<int:recipe_id>/', StoreRecipeDeleteView.as_view(), name='store-recipe-delete'),
+    # 특정 상점의 모든 레시피 조회 (GET), 레시피 생성 (POST)
+    path('<int:store_id>/', StoreRecipeListView.as_view(), name='store-recipe-list-create'),
+
+    # 특정 레시피 조회 (GET), 수정 (PUT), 삭제 (DELETE)
+    path('<int:store_id>/<int:recipe_id>/', StoreRecipeDetailView.as_view(), name='store-recipe-detail'),
 ]
