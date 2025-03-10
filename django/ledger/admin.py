8e99925af729cacea239c38051728542d5ce7846
youngkwangjoo <dudrknd1642@gmail.com>
2025-03-10 19:46:15 +0900

    fix: ledger admin.py

--- django/ledger/admin.py
+++ django/ledger/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Category  # ✅ Transaction 삭제
+from store.models import Transaction
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
     list_display = ["id", "store", "category", "transaction_type", "amount", "date"]
     list_filter = ["transaction_type", "store"]
     search_fields = ["category__name", "store__name"]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # ✅ Admin 리스트에서 보이는 컬럼
+    search_fields = ('name',)  # ✅ 검색 기능 추가
