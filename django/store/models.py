083d8c5405fc5c254ce80e2a1ca187c070462202
youngkwangjoo <dudrknd1642@gmail.com>
2025-03-10 21:19:28 +0900

    fix: store category 수정

--- django/store/models.py
+++ django/store/models.py
@@ -23,7 +23,7 @@ class Transaction(models.Model):
     store = models.ForeignKey('Store', on_delete=models.CASCADE, related_name='transactions', default=1)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # 금액
     transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)  # 수입 또는 지출 구분
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)  # 카테고리와 연결
+    category = models.ForeignKey("ledger.Category", on_delete=models.SET_NULL, null=True, blank=True) 
     date = models.DateField()  # 거래 발생 날짜
     description = models.TextField(blank=True, null=True)  # 설명 (선택 사항)
     created_at = models.DateTimeField(auto_now_add=True)  # 생성 시간
