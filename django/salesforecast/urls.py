5a910445a44b75f2d23bf14c50f0a54afbdb52e5
youngkwangjoo <dudrknd1642@gmail.com>
2025-03-12 01:58:30 +0900

    fix: 주석처리

--- django/salesforecast/urls.py
+++ django/salesforecast/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import SalesForecastView
+# from django.urls import path
+# from .views import SalesForecastView
 
-urlpatterns = [
-    path("predict/", SalesForecastView.as_view(), name="sales_forecast"),
-]
+# urlpatterns = [
+#     path("predict/", SalesForecastView.as_view(), name="sales_forecast"),
+# ]
