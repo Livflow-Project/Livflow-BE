7a5099c40c3edca5b582c921f7ffd32bc8d613ea
youngkwangjoo <dudrknd1642@gmail.com>
2025-03-17 00:46:54 +0900

    feat: ingredientusage view 추가

--- django/ingredients/urls.py
+++ django/ingredients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import StoreIngredientView, IngredientDetailView
+from .views import StoreIngredientView, IngredientDetailView, IngredientUsagesView
 
 urlpatterns = [
     path('<uuid:store_id>/', StoreIngredientView.as_view(), name='store-ingredients'),  # ✅ UUID 적용
     path('<uuid:store_id>/<uuid:ingredient_id>/', IngredientDetailView.as_view(), name='ingredient-detail'),  # ✅ UUID 적용
+    path('ingredients/<uuid:store_id>/<uuid:ingredient_id>/usages/', IngredientUsagesView.as_view(), name='ingredient-usages'),
 ]
